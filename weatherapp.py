ea8f0d365a416e4c873343eb8344a722fa038013
om3rta <spamitycalamity@gmail.com>
2014-05-28 19:19:31 -0400

    Do work
    weatherapp.py can now pull info from weather underground. Need to figure
    out how to create a dictionary from the info gathered.

--- weatherapp.py
+++ weatherapp.py
@@ -1,1 +1,15 @@
 #Starting app
+import pycurl
+import cStringIO
+ 
+buf = cStringIO.StringIO()
+ 
+c = pycurl.Curl()
+c.setopt(c.URL, 'http://api.wunderground.com/api/3ff15933e8683503/conditions/q/CA/San_Francisco.json')
+c.setopt(c.WRITEFUNCTION, buf.write)
+c.perform()
+ 
+weather_info = buf.getvalue()
+buf.close()
+
+print weather_info["response"]
