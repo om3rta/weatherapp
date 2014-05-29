42b797cc645eb8a07c980d2ee61a8891c6ff9c82
Zack <spamitycalamity@gmail.com>
2014-05-29 13:08:55 -0400

    weatherapp v1 completed
    weatherapp can now successfully take user input and print out weather
    conditions for the specified location.

--- testapp.py
+++ testapp.py
@@ -1,94 +1,17 @@
-weather_info = {
-  "response": {
-  "version":"0.1",
-  "termsofService":"http://www.wunderground.com/weather/api/d/terms.html",
-  "features": {
-  "conditions": 1
-  }
-        }
-  ,     "current_observation": {
-                "image": {
-                "url":"http://icons.wxug.com/graphics/wu2/logo_130x80.png",
-                "title":"Weather Underground",
-                "link":"http://www.wunderground.com"
-                },
-                "display_location": {
-                "full":"San Francisco, CA",
-                "city":"San Francisco",
-                "state":"CA",
-                "state_name":"California",
-                "country":"US",
-                "country_iso3166":"US",
-                "zip":"94101",
-                "magic":"1",
-                "wmo":"99999",
-                "latitude":"37.77500916",
-                "longitude":"-122.41825867",
-                "elevation":"47.00000000"
-                },
-                "observation_location": {
-                "full":"SOMA - Near Van Ness, San Francisco, California",
-                "city":"SOMA - Near Van Ness, San Francisco",
-                "state":"California",
-                "country":"US",
-                "country_iso3166":"US",
-                "latitude":"37.773285",
-                "longitude":"-122.417725",
-                "elevation":"49 ft"
-                },
-                "estimated": {
-                },
-                "station_id":"KCASANFR58",
-                "observation_time":"Last Updated on May 28, 3:37 PM PDT",
-                "observation_time_rfc822":"Wed, 28 May 2014 15:37:34 -0700",
-                "observation_epoch":"1401316654",
-                "local_time_rfc822":"Wed, 28 May 2014 15:38:16 -0700",
-                "local_epoch":"1401316696",
-                "local_tz_short":"PDT",
-                "local_tz_long":"America/Los_Angeles",
-                "local_tz_offset":"-0700",
-                "weather":"Clear",
-                "temperature_string":"69.7 F (20.9 C)",
-                "temp_f":69.7,
-                "temp_c":20.9,
-                "relative_humidity":"48%",
-                "wind_string":"From the SSE at 11.0 MPH",
-                "wind_dir":"SSE",
-                "wind_degrees":160,
-                "wind_mph":11.0,
-                "wind_gust_mph":0,
-                "wind_kph":17.7,
-                "wind_gust_kph":0,
-                "pressure_mb":"1017",
-                "pressure_in":"30.03",
-                "pressure_trend":"-",
-                "dewpoint_string":"49 F (9 C)",
-                "dewpoint_f":49,
-                "dewpoint_c":9,
-                "heat_index_string":"NA",
-                "heat_index_f":"NA",
-                "heat_index_c":"NA",
-                "windchill_string":"NA",
-                "windchill_f":"NA",
-                "windchill_c":"NA",
-                "feelslike_string":"69.7 F (20.9 C)",
-                "feelslike_f":"69.7",
-                "feelslike_c":"20.9",
-                "visibility_mi":"10.0",
-                "visibility_km":"16.1",
-                "solarradiation":"--",
-                "UV":"10","precip_1hr_string":"0.00 in ( 0 mm)",
-                "precip_1hr_in":"0.00",
-                "precip_1hr_metric":" 0",
-                "precip_today_string":"0.00 in (0 mm)",
-                "precip_today_in":"0.00",
-                "precip_today_metric":"0",
-                "icon":"clear",
-                "icon_url":"http://icons.wxug.com/i/c/k/clear.gif",
-                "forecast_url":"http://www.wunderground.com/US/CA/San_Francisco.html",
-                "history_url":"http://www.wunderground.com/weatherstation/WXDailyHistory.asp?ID=KCASANFR58",
-                "ob_url":"http://www.wunderground.com/cgi-bin/findweather/getForecast?query=37.773285,-122.417725"
-        }
-}
+import pycurl
+import cStringIO
+import json
 
-print weather_info
+
+import pycurl
+import cStringIO
+ 
+buf = cStringIO.StringIO()
+ 
+c = pycurl.Curl()
+c.setopt(c.URL, "http://api.wunderground.com/api/3ff15933e8683503/conditions/q/48198.json")
+c.setopt(c.WRITEFUNCTION, buf.write)
+c.perform()
+ 
+print buf.getvalue()
+buf.close()
--- weatherapp.py
+++ weatherapp.py
@@ -1,15 +1,60 @@
 #Starting app
 import pycurl
 import cStringIO
- 
-buf = cStringIO.StringIO()
- 
-c = pycurl.Curl()
-c.setopt(c.URL, 'http://api.wunderground.com/api/3ff15933e8683503/conditions/q/CA/San_Francisco.json')
-c.setopt(c.WRITEFUNCTION, buf.write)
-c.perform()
- 
-weather_info = buf.getvalue()
-buf.close()
+import json
+from pprint import pprint
+import os
 
-print weather_info["response"]
+#get_input asks user for zip code/city
+def get_input():
+	city = ""
+	city = raw_input(["enter a zip code, or enter 'exit' to exit"])
+	city = city.lower()
+	return city
+
+#make_url constructs a URL based on the user input
+def make_url(city):
+	url_front = "http://api.wunderground.com/api/3ff15933e8683503/conditions/q/"
+	url_city = city
+	url_end = ".json"
+	url_full = url_front + city + url_end
+	return url_full
+
+#get_weather uses the contructed URL from make_url to request information from the wunderground API
+def get_weather(new_url):
+	buf = cStringIO.StringIO()
+	c = pycurl.Curl()
+	c.setopt(c.URL, new_url)
+	c.setopt(c.WRITEFUNCTION, buf.write)
+	c.perform()
+	weather_info = buf.getvalue()
+	buf.close()
+	return weather_info
+
+#organize_weather parses the information gathered by get_weather and organizes it into a dictionary
+def organize_weather(forecast):
+	weather_parsed = json.loads(forecast)
+	return weather_parsed
+
+#weather_report gathers information from the dictionary created in organize_weather and prints it
+def weather_report():
+	report = ""
+	print "\nCurrent weather report for " + forecast_json['current_observation']['display_location']['city'] + ", " + forecast_json['current_observation']['display_location']['state'] + ":\n\n"
+	print "Condition: " + forecast_json['current_observation']['weather']
+	print "Temperature: " + forecast_json['current_observation']['temperature_string']
+	print "Feels like: " + forecast_json['current_observation']['feelslike_string']
+	return report
+
+#main program. Prompt user for input. Gather weather information from specified location.
+#screen clears every time the program starts, or when a new report is done. Reports can be repeated until the user chooses to 'exit'.
+city = None
+os.system('cls' if os.name == 'nt' else 'clear')
+while city != "exit":
+	city = get_input()
+	if city != "exit":
+		os.system('cls' if os.name == 'nt' else 'clear')
+		new_url = make_url(city)
+		forecast = get_weather(new_url)
+		forecast_json = organize_weather(forecast)
+		report = weather_report()
+		print report
